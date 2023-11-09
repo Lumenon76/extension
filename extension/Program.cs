24fdf025b7775ae1608652b40ed0ce1ce2f8c635
Lumenon76 <lumenon76@gmail.com>
2023-11-09 20:41:02 +0100

    Ajoutez des fichiers projet.

---
+++ extension/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
